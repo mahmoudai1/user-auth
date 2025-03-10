0fc19bda1eb1a840446c03ffd7fc16f83e44670b
Mahmoud Ahmed <66283081+mahmoudai1@users.noreply.github.com>
2025-03-10 02:08:05 +0200

    DRY

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -19,45 +19,40 @@ const Dashboard: React.FC = () => {
           const response = await verify(session);
           if(response.status != 200){
             destruct();
-            navigate('/');
           }
         } else {
           destruct();
-          navigate('/');
         }
       } catch {
         destruct();
-        navigate('/');
       }
-      
     };
-
     fetchDashboard();
-  }, [navigate]);
+  });
 
   const handleLogout = () => {
     logout();
     destruct();
-    navigate('/');
   };
 
   const destruct = () => {
     Cookies.remove('session');
     Cookies.remove('username');
+    navigate('/');
   }
 
   return (
-    <div className="m-10 p-5 bg-gray-50 border border-gray-200 shadow rounded-2xl md:flex md:items-center md:justify-between">
-      <div className="min-w-0 flex-1">
-        <h2 className="text-2xl/7 font-bold text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
+    <div className="p-5 m-10 border border-gray-200 shadow bg-gray-50 rounded-2xl md:flex md:items-center md:justify-between">
+      <div className="flex-1 min-w-0">
+        <h2 className="font-bold text-gray-900 text-2xl/7 sm:truncate sm:text-3xl sm:tracking-tight">
           Welcome, {username}!
         </h2>
       </div>
-      <div className="mt-4 flex md:ml-4 md:mt-0">
+      <div className="flex mt-4 md:ml-4 md:mt-0">
         <button
           type="button"
           onClick={handleLogout}
-          className="ml-3 inline-flex items-center rounded-md bg-red-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          className="inline-flex items-center px-3 py-2 ml-3 text-sm font-semibold text-white bg-red-500 rounded-md shadow-sm hover:bg-red-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
         >
           Logout
         </button>
